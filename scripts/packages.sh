dbc6afbabb76c1d9f1b270cbab80e6aa42dec6da
Michael F <penguinmenac3@gmail.com>
2016-01-11 23:15:42 +0100

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,4 +3,4 @@
 # Install packages
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y git cinnamon openssh-server htop tmux sl chromium-browser cmake gimp ipython josm keepass2 texmaker virtualbox vlc qtcreator spotify-client numix-gtk-theme numix-icon-theme-circle numix-wallpaper-* unity-tweak-tool gnome-tweak-tool python-wxgtk2.8 steam ninja-ide
+sudo apt-get install -y git cinnamon openssh-server htop tmux sl chromium-browser cmake gimp ipython josm keepass2 texmaker virtualbox vlc qtcreator spotify-client numix-gtk-theme numix-icon-theme-circle numix-wallpaper-* unity-tweak-tool gnome-tweak-tool python-wxgtk2.8 steam ninja-ide python-pip python-pyglet
